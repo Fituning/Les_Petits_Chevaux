411eed2de972de960367c0e53b6d5c2862a23a6d
Arfentis <celianh@club-internet.fr>
2021-03-30 21:43:57 +0200

    test (ajout de setX et setY a la classe position)

--- src/Position.java
+++ src/Position.java
@@ -3,4 +3,14 @@ package Les_Petits_Chevaux.src;
 public class Position {
     private int x;
     private int y;
+
+    public void setX(int x){
+        this.x=x;
+    }
+    
+    public void setY(int y){
+        this.y=y;
+    }
+
 }
+
