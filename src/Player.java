caa6330df5dc17dc166e959f3522be471cfaedf6
Fituning <carstar@live.fr>
2021-03-30 14:28:26 +0200

    modif UML

--- src/Player.java
+++ src/Player.java
@@ -4,55 +4,34 @@ import java.awt.*;
 
 public class Player {
     private String pseudo;
-    private Color teamColor;
-    private int points;
-    private Piece[] piecesArray;
+    private Team team;
 
 
     /* definition of the construct method*/ 
+
     Player(String pseudo, Color teamColor){
         this.pseudo = pseudo;
-        this.teamColor = teamColor;
-        this.points = 0;
-        this.piecesArray = newPieces(teamColor);
+        this.team = new Team(teamColor);
     }
 
     /* player stters */
-
-    public void setPoints(int points) {
-        this.points = points;
-    }
 
     public void setPseudo(String pseudo) {
         this.pseudo = pseudo;
     }
 
-    public void setTeamColor(Color teamColor) {
-        this.teamColor = teamColor;
+    public void setTeam(Color teamColor) {
+        this.team = new Team(teamColor);
     }
 
     /* player getters */
 
-    public Piece[] getPieces() {
-        return piecesArray;
-    }
-    public Color getTeamColor() {
-        return teamColor;
-    }
-    public int getPoints() {
-        return points;
-    }
     public String getPseudo() {
         return pseudo;
     }
 
-
-    /* creation of an Array of Pieces */
-
-    private Piece[] newPieces(Color teamColor){
-        Piece[] pieces = new Piece[4];
-        /*declaration of the 4 new Pieces */
-        return pieces;
+    public Team getTeam() {
+        return team;
     }
 
 }
---
+++ src/Team.java
@@ -0,0 +1,52 @@
+package Les_Petits_Chevaux.src;
+
+import java.awt.*;
+
+public class Team {
+    private Color teamColor;
+    private int points;
+    private Piece[] piecesArray;
+
+
+    /* definition of the construct method*/ 
+    Team(Color teamColor){
+        this.teamColor = teamColor;
+        this.points = 0;
+        this.piecesArray = newPieces(teamColor);
+    }
+
+    /* player stters */
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public void setPiecesArray(Piece[] piecesArray) {
+        this.piecesArray = piecesArray;
+    }
+
+    public void setTeamColor(Color teamColor) {
+        this.teamColor = teamColor;
+    }
+
+    /* player getters */
+
+    public Piece[] getPieces() {
+        return piecesArray;
+    }
+    public Color getTeamColor() {
+        return teamColor;
+    }
+    public int getPoints() {
+        return points;
+    }
+
+
+    /* creation of an Array of Pieces */
+
+    private Piece[] newPieces(Color teamColor){
+        Piece[] pieces = new Piece[4];
+        /*declaration of the 4 new Pieces */
+        return pieces;
+    }
+}
