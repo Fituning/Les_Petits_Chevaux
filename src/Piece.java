90f5fe444a8b406132d615c56b5c61dd4463fd0e
Arfentis <celianh@club-internet.fr>
2021-04-07 23:44:25 +0200

    add a constructor and int id for test purposes

--- src/Piece.java
+++ src/Piece.java
@@ -1,9 +1,13 @@
 package Les_Petits_Chevaux.src;
 
-import java.awt.*;
+import java.awt.Color;
+
+import Les_Petits_Chevaux.Path;
+import Les_Petits_Chevaux.Piece;
 
 public class Piece {
     private Color color;
+    private int id;
     private Path piecePath;
     private boolean pathCompleted;
     private int currentPosition;
@@ -12,35 +16,43 @@ public class Piece {
 Piece(Color color){
         this.color=color;
         this.pathCompleted=false;
-        this.Path.inPist=false;
-        this.nbOfCaseTraveled=0;
+        this.piecePath.setInPist(false);
+        this.id=0;
+        this.currentPosition=0;
 }
+
+Piece(int id){
+    this.pathCompleted=false;
+    this.piecePath.setInPist(false);
+    this.id=id;
+    this.currentPosition=1;
+}
+
 //could be use if a pawn must be set to a particular square
 public void setPiecePosition(int pos){
-    this.Path.pathPosition=pos;
+    this.currentPosition=pos;
 }
 
 
-
-
-
-//check if 2 pawn are one the same square (should check for the 4 pawn but we should still be able to find wich pawn is on the same squre to send it home)
-public boolean comparePawnPosition(Piece p1,Piece p2){
-    if(p1.getPathPosition==p2.getPathPosition){
-        return true;
-    }
-    else
-        return false;
+public int getId() {
+	return id;
+}
+public void setId(int nb) {
+	this.id = nb;
 }
 
 //check if a dice roll correponding to a pawn is equal to 6 and if this is th case set the "inPist" value of the pawn to true
 public void outside(Piece p1,int di){
     if(di==6){
-        p1.Path.inPist=true;
+        p1.piecePath.setInPist(true);
     }
 }
     public int getcurrentPosition(){
         return this.currentPosition;
+    }
+    
+    public void setCurrentPosition(int currentPosition) {
+    	this.currentPosition = currentPosition;
     }
 
     public Color getColor() {
@@ -66,10 +78,6 @@ public void outside(Piece p1,int di){
     public void setPathCompleted(boolean pathCompleted) {
         this.pathCompleted = pathCompleted;
     }
-
-
-
-
 
 
 }
