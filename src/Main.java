779b6091056e8431bc963d9ecb5fbcb8e3103eb0
Fituning <carstar@live.fr>
2021-04-07 12:04:23 +0200

    initpath

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package Les_Petits_Chevaux.src;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Square[] squareArray = new Square[76];
+        squareArray = initSquares();
+
+    }
+}
