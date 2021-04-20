782fde93dd5258e5d5053428d3b4d76eb9b5f8e1
Arfentis <celianh@club-internet.fr>
2021-04-20 19:55:43 +0200

    actual initialisation of the player

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,7 @@
 package Les_Petits_Chevaux.src;
 
 import java.awt.*;
+import java.util.Scanner;
 
 public class Player {
     private String pseudo;
@@ -12,6 +13,12 @@ public class Player {
     Player(String pseudo, Color teamColor){
         this.pseudo = pseudo;
         this.team = new Team(teamColor);
+    }
+
+    /* definition of the default construct method*/
+    Player(){
+        this.pseudo = "john";
+        this.team =new Team(Color.BLACK); 
     }
 
     /* player setters */
@@ -34,5 +41,47 @@ public class Player {
         return team;
     }
 
-}
+    public void initPlayers(Player pl1,int nb){
+        System.out.print("chose your nickname: ");
+        String scra;
+         Scanner sc = new Scanner( System.in ); 
+             scra = sc.nextLine();
+             pl1.setPseudo(scra);
+             System.out.print(pl1.getPseudo());
+            //CHANGER AVEC L4AFFICHAGE GRAPHIQUE
+            
+            
+              switch(nb) {
+                case 1:
+                    pl1.setTeam(Color.RED);
+                    System.out.print("You are in the RED team \n");
+                    pl1.team.newPieces(Color.RED);
+                  break;
+                case 2:
+                    pl1.setTeam(Color.BLUE); 
+                    System.out.print("You are in the BLUE team \n");
+                    pl1.team.newPieces(Color.BLUE);
+                  break;
+                case 3:
+                    pl1.setTeam(Color.GREEN);
+                    System.out.print("You are in the GREEN team \n");
+                    pl1.team.newPieces(Color.GREEN);
+                  break;
+                case 4:
+                    pl1.setTeam(Color.YELLOW);
+                    System.out.print("You are in the YELLOW team \n");
+                    pl1.team.newPieces(Color.YELLOW);
+
+                default:
+                  // CHANGER C4EST DE LA MERDE
+                }
+                System.out.print(pl1.getPseudo());
+                System.out.print(pl1.getTeam().getTeamColor());
+                sc=null;
+            
+            }
+    }
+
+    
+
 
