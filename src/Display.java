5c8f7949b8bfb345130dc7ecae37085e0f659411
Arfentis <celianh@club-internet.fr>
2021-04-20 19:53:42 +0200

    ADD display class for test purpuses

---
+++ src/Display.java
@@ -0,0 +1,29 @@
+package Les_Petits_Chevaux.src;
+
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/*public class Display  extends JFrame {
+  
+  private static final long serialVersionUID = 1L;
+  
+  public Display(String titre) {
+    super(titre);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    init();
+  }
+  
+  private void init()
+  {
+    JLabel label = new JLabel(new ImageIcon("I:/Travail/java/vs/Ludo/Les_Petits_Chevaux/resources/Plateau_numero.png") );
+    this.add(label, BorderLayout.CENTER);
+    this.pack();    
+  }
+  
+  public static void main(String[] args) {
+    Display app = new Display("Afficher image");
+    app.setVisible(true);
+  }
+}*/
